9bc7493db6f472a964325e6e0378c0130cf677b9
Sachin Ahirkar <sachlinux@gmail.com>
2018-03-20 12:00:19 +0530

    another code

---
+++ exercise/comb.py
@@ -0,0 +1,12 @@
+#list string tuples
+
+my_list=['sunday','monday','tuesday']
+my_string="%s hello janu how r u"
+my_tuple=(1,2,3,4,5)
+print(my_list)
+print(my_string)
+print(my_tuple)
+print(my_list[2])
+print(my_string %1000)
+print(my_tuple[2])
+
